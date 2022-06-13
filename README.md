78855bb07f2fa09e9fd0f09a0a60a3b58bb91768
unknown <samiaabdelhaleem@gmail.com>
2022-06-13 22:31:16 +0200

    generate the degress of the color

---
+++ README.md
@@ -0,0 +1,3 @@
+## Idea
+
+enter the hexa code of the color and it will generate the degrees
---
+++ src/SingleColor.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
+
+const SingleColor = ({ rgb, weight, index }) => {
+  const [alert, setAlert] = useState(false);
+  //convert from array rgb: (3) [255, 255, 255] to string
+  const bcg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeOut);
+  }, [alert]);
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{
+        backgroundColor: `rgb(${bcg})`,
+      }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex);
+      }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hex}</p>
+      {alert && <p className="alert"> copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
